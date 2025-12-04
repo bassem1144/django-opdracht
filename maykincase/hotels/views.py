062a17800fc7e51dfe0e9272e78bca8dce190c9a
Bassem Ismail <bassem.ismail@hva.nl>
2025-12-04 23:33:36 +0100

    Add hotel list view with city filtering

--- maykincase/hotels/views.py
+++ maykincase/hotels/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Hotel, City
 
-def test_view(request):
-    return HttpResponse("Hello World!")
+# Display all hotels, optionally filtered by city
+def hotel_list(request):
+    # Get the selected city code from the query parameters (if not blank)
+    selected_city = request.GET.get('city', '')
+
+    # Get all cities in the database
+    cities = City.objects.all().order_by('name')
+
+    # Get all hotels and filter by city if selected
+    hotels = Hotel.objects.select_related('city')
+    if selected_city:
+        hotels = hotels.filter(city__code=selected_city)
+    
+    return render(request, 'hotel_list.html', {
+        'hotels': hotels,
+        'cities': cities,
+        'selected_city': selected_city
+    })
